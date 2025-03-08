f0a9a105efba878f1fffa7ca5b3574e0e009521b
Miss-Simra <simraketheeswaran@gmail.com>
2025-03-08 12:32:29 +0100

    Remove button 'arrow' and correct path

--- structure/footer.php
+++ structure/footer.php
@@ -16,13 +16,8 @@
 
     <!-- Boutons à droite -->
     <div class="footer-buttons d-flex gap-3">
-        <a href="#haut" class="footer-btn">
-            <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 384 512">
-                <path fill="#FFFFFF" d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/>
-            </svg>
-        </a>
-        <a href="pages/connexion.php" class="footer-btn">Compte</a>
-        <a href="pages/contact.php" class="footer-btn">Contact</a>
+        <a href="connexion.php" class="footer-btn">Compte</a>
+        <a href="contact.php" class="footer-btn">Contact</a>
     </div>
 
 </footer>
