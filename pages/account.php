59fad34ab97addb09d22418ace1f76bc2da8b5ad
Miss-Simra <simraketheeswaran@gmail.com>
2025-03-21 15:12:12 +0100

    MAJ info and session

--- pages/account.php
+++ pages/account.php
@@ -1,16 +1,25 @@
-<?php include '../structure/header.php'; ?>
-
 <?php 
     // Initialisation/relaye la session
     session_start();
 
-    // Définitions des fonctions utilisées plusieurs fois 
-    require '../inc/inc.functions.php';
+    if (!isset($_SESSION['user_id'])) {
+        header('Location: login.php');
+        exit;
+    }
 
 ?>
-	
+
+<?php include '../structure/header.php'; ?>
+
+	<div class="container">
+
+    <div class="d-flex-column justify-content-center align-items-center">
+    <h1>Vos informations personnelles</h1>
+    
     
     <a href="disconnect.php" class="btn btn-danger m-2">Se déconnecter</a>
+    </div>
+    </div>
 
 
 <?php include '../structure/footer.php'; ?>
