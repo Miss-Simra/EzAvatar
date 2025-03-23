6f01cc4f153a10b6e65cbb92d470455504c22e28
Miss-Simra <simraketheeswaran@gmail.com>
2025-03-23 23:27:23 +0100

    MAJ structure

--- pages/admin.php
+++ pages/admin.php
@@ -7,3 +7,30 @@ $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
 
 ?>
+
+<?php include '../structure/header.php'; ?>
+
+<section class="bg_account"> 
+
+	<div class="container">
+    <div class="form_login">
+    <div class="d-flex justify-content-between">
+    <h1 class="h1_account">Gestion des utilisateurs</h1> 
+    <a href="disconnect.php" class="btn btn-danger m-2">Se déconnecter</a>
+    </div>
+
+    <div class="info_account">
+        <div class="d-flex align-items-center">
+
+        </div>
+        <div class="d-flex align-items-center">
+
+        </div>
+    </div>
+
+    </div>
+    </div>
+
+</section> 
+
+<?php include '../structure/footer.php'; ?>
