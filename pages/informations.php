baafdec5df9323025f68fd5e84ad6bf4ed9b5894
Miss-Simra <simraketheeswaran@gmail.com>
2025-04-04 12:43:13 +0200

    MAJ structure

--- pages/informations.php
+++ pages/informations.php
@@ -1,10 +1,31 @@
-
-<?php
-
-session_start();
-
-?>
-
 <?php include '../structure/header.php'; ?>
+
+<div class="bg_info">
+    
+    <div class="container mt-5 bg-white p-5 rounded-3">
+
+<h2 class="m-3">Informations sur EzAvatar</h2>
+    <p class="m-3 text-justify">Bienvenue sur EzAvatar, votre générateur d'avatars personnalisés et amusants.</p>
+    
+    <h2 class="m-3">Qui sommes-nous ?</h2>
+    <p class="m-3 text-justify">EzAvatar est une plateforme permettant aux utilisateurs de créer des avatars uniques à partir de divers styles et options de personnalisation.</p>
+    
+    <h2 class="m-3">Fonctionnalités principales</h2>
+    <ul>
+        <li>Génération d'avatars personnalisés</li>
+        <li>Choix de couleurs et de styles</li>
+        <li>Téléchargement en différents formats (PNG et JPEG)</li>
+    </ul>
+    
+    <h2 class="m-3">Comment ça marche ?</h2>
+    <p class="m-3 text-justify">1. Sélectionnez un style d'avatar.<br>
+       2. Personnalisez les couleurs et options.<br>
+       3. Téléchargez votre avatar !</p>
+    
+    <h2 class="m-3">Contact</h2>
+    <p class="m-3 text-justify">Pour toute question, contactez-nous à : <a href="">contact@ezavatar.com</a></p>
+
+</div>
+</div>
 
 <?php include '../structure/footer.php'; ?>
